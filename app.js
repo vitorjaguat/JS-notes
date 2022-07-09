83975f9c37ca4367ae06479ef0838ab51932b097
Vitor Jaguat <vitorbutkus@gmail.com>
2022-07-09 20:10:04 -0300

    Add content, the find Method

--- app.js
+++ app.js
@@ -984,9 +984,96 @@
 
 //Jonas sc 10 challenge 2
 //My solution:
-(function () {
-    const header = document.querySelector('h1'); header.style.color = 'red';
-    document.body.addEventListener('click', function () {
-        header.style.color = 'blue'
-    })
-})();
+// (function () {
+//     const header = document.querySelector('h1'); header.style.color = 'red';
+//     document.body.addEventListener('click', function () {
+//         header.style.color = 'blue'
+//     })
+// })();
+
+//Jonas sc11 challenge 1
+// Coding Challenge #1
+// Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy.
+// A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+// Your tasks:
+// Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+// 1. Juliafoundoutthattheownersofthefirstandthelasttwodogsactuallyhave cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+// 2. CreateanarraywithbothJulia's(corrected)andKate'sdata
+// 3. Foreachremainingdog,logtotheconsolewhetherit'sanadult("Dognumber1
+// is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy   ")
+// 4. Runthefunctionforbothtestdatasets
+// Test data:
+// § Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3] § Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+// Hints: Use tools from all lectures in this section so far 😉 GOOD LUCK 😀
+// Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+// Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+//My solution:
+// const julia1 = [3, 5, 2, 12, 7]
+// const kate1 = [4, 1, 15, 8, 3]
+// const julia2 = [9, 16, 6, 8, 3]
+// const kate2 = [10, 5, 6, 1, 4]
+
+// const checkDogs = function (arr1, arr2) {
+//     const jDogs = arr1.slice(1).slice(0, -2);
+//     const kDogs = arr2;
+//     console.log(jDogs, kDogs)
+//     jDogs.forEach(function (dog, i) {
+//         dog >= 3 ? console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`) : console.log(`Dog number ${i + 1} is still a puppy`)
+//     });
+//     kDogs.forEach(function (dog, i) {
+//         dog >= 3 ? console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`) : console.log(`Dog number ${i + 1} is still a puppy`)
+//     });
+// }
+// checkDogs(julia1, kate1)
+
+//Jonas sc 11 challenge 2
+// Coding Challenge #2
+// Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+// Your tasks:
+// Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+// 1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4
+// 2. Exclude all dogs that are less than 18 human years old(which is the same as keeping dogs that are at least 18 years old)
+// 3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+// 4. Run the function for both test datasets
+// Test data:
+// § Data1:[5,2,4,1,15,8,3] § Data2:[16,6,10,5,6,1,4]
+// GOOD LUCK 😀
+
+//MY SOLUTION:
+// const Data1 = [5, 2, 4, 1, 15, 8, 3]
+// const Data2 = [16, 6, 10, 5, 6, 1, 4]
+
+// const calcAverageHumanAge = function (ages) {
+//     const humAge = ages.map(function (age) {
+//         return age <= 2 ? age * 2 : 16 + age * 4;
+//     });
+//     const adults = humAge.filter(function (age) {
+//         return age >= 18;
+//     });
+//     const avg = adults.reduce(function (acc, age) {
+//         return acc + age;
+//     }, 0) / adults.length;
+//     //const avg = adults.reduce(function (acc, age) {
+//     //return acc + age;
+// }, 0) / adults.length;
+
+// console.log(avg)
+// }
+
+// calcAverageHumanAge(Data1)
+// calcAverageHumanAge(Data2)
+
+//Jonas sc 11 challenge 3
+//Rewrite the previous challenge solution using chaining and arrow functions:
+
+const Data1 = [5, 2, 4, 1, 15, 8, 3]
+const Data2 = [16, 6, 10, 5, 6, 1, 4]
+
+const calcAverageHumanAge = function (ages) {
+    return ages.map(age => age <= 2 ? age * 2 : 16 + age * 4)
+        .filter(age => age >= 18)
+        .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+}
+
+console.log(calcAverageHumanAge(Data1))
+console.log(calcAverageHumanAge(Data2))
